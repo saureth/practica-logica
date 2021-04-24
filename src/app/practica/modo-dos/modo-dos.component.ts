b9d6fdc5035092e46eeb1199db0b367877c140d1
Valentina Cadena <valentina.cadenaz@udea.edu.co>
2021-04-23 23:21:21 -0500

    [FEAT] se agregan metodos para que el usuario adivine

--- src/app/practica/modo-dos/modo-dos.component.ts
+++ src/app/practica/modo-dos/modo-dos.component.ts
@@ -12,6 +12,13 @@ export class ModoDosComponent implements OnInit {
   modoDosFormGroup: any;
   numeroUsuario: any;
   numeroMaquina: any;
+  listaUsuario: any;
+  resultadoUltimoIntento: string="";
+  cantidadDigitos: number = 4;
+  datos: any[] = [];
+  adivinoUsuario = false;
+  adivinoMaquina = false;
+  datosTabla: any[] = [];
   
   constructor(private readonly formBuilder: FormBuilder) { }
 
@@ -35,6 +42,81 @@ export class ModoDosComponent implements OnInit {
         Validators.min(0),
         Validators.max(9999)]
       ]
+    });
+  }
+
+  crearListaConNumero(numero: string): boolean{
+    let _invalido = false;
+    let _cantidad = 0;
+    this.listaUsuario = new ListaSimple();
+    this.listaUsuario.añadirAlFinal(Number.parseInt(numero[0]));
+    _cantidad ++;
+    for (let index = 1; index < numero.length; index++) {
+      const digito = Number.parseInt(numero[index]);
+      if(this.listaUsuario.buscarNumero(digito)){ 
+        index = numero.length;
+        _invalido = true;
+      }
+      else{
+        this.listaUsuario.añadirAlFinal(digito);
+        _cantidad ++;
+      }
+    }
+    if (_cantidad != 4 ) {
+      _invalido = true;
+    }
+    return _invalido;
+  }
+
+  comparar() {
+    let _sUsuario: string = this.modoDosFormGroup.get("numeroAdivinarUsuario").value.toString(); // obtengo los 4 dígitos del usuario como string
+    let _invalido = this.crearListaConNumero(_sUsuario);
+    if(_invalido){
+      this.resultadoUltimoIntento = "El número es inválido, por favor revise";
+    }
+    else {
+      let _picas: number = 0;
+      let _fijas: number = 0;
+      for (let index = 0; index < this.cantidadDigitos; index++) { // itero sobre cada uno de esos 4 dígitos
+        let _nUsuario = this.listaUsuario?.retornaNumeroEnPosicion(index); //tomo el dígito en esa posición y lo vuelvo un número
+        let posicionEncontrado = this.numeroMaquina?.buscarPosicionNumero(_nUsuario); // busco si en la lista está ese # y retorno la posición
+        if (posicionEncontrado == -1) { // Esto es si el dígito no está en ninguna posición de la lista
+          //console.log("No hay ni pica ni fija");
+        }
+        else if (index == posicionEncontrado) { // Esto es si el dígito está en la lista y en la misma posición
+          //console.log("Obtiene fija");
+          _fijas++;
+        } else { // Esto es si el dígito sí está en la lista pero en una posición diferente
+          //console.log("Obtiene pica");
+          _picas++;
+        }
+      }
+      this.resultadoUltimoIntento = //"Intento # "+  (this.cuentaTurnos + 1) 
+         "Obtuvo " + _picas + " picas y " + _fijas + " fijas"; 
+
+      this.datos.push({
+        numeroUsuario: Number.parseInt(_sUsuario),
+        picas: _picas,
+        fijas: _fijas
+      });
+
+      if (_fijas == 4) {
+        console.log("adivino");
+      //this.cuentaTurnos = this.turnos;
+        //this.termino = true;
+        this.adivinoUsuario = true;
+        this.resultadoUltimoIntento += " ¡¡ Adivinó !! "
+      } else {
+        //this.cuentaTurnos++;
+      }
+      this.actualizarDatos();
+    }
+  }
+
+  actualizarDatos(){
+    this.datosTabla= [];
+    this.datos.forEach(dato => {
+      this.datosTabla.push(dato);
     });
   }
 
