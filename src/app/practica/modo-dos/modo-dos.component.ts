b7f57162f1ef21181d2b267ebf31b399996e5ba3
Santiago Sanmartín <aerials_001@hotmail.com>
2021-04-27 21:50:59 -0500

    [REFACTOR] Se cambian de lugar algunas partes del código para hacerlo más legible

--- src/app/practica/modo-dos/modo-dos.component.ts
+++ src/app/practica/modo-dos/modo-dos.component.ts
@@ -53,55 +53,55 @@ export class ModoDosComponent implements OnInit {
     });
   }
 
-  comparar() {
+  comparar(lista: ListaSimple){
+    let _picas: number = 0;
+    let _fijas: number = 0;
+    for (let index = 0; index < this.cantidadDigitos; index++) { // itero sobre cada uno de esos 4 dígitos
+      let _nUsuario = lista.retornaNumeroEnPosicion(index); //tomo el dígito en esa posición y lo vuelvo un número
+      let posicionEncontrado = this.numeroMaquina?.buscarPosicionNumero(_nUsuario); // busco si en la lista está ese # y retorno la posición
+      if (posicionEncontrado == -1) { // Esto es si el dígito no está en ninguna posición de la lista
+        //console.log("No hay ni pica ni fija");
+      }
+      else if (index == posicionEncontrado) { // Esto es si el dígito está en la lista y en la misma posición
+        //console.log("Obtiene fija");
+        _fijas++;
+      } else { // Esto es si el dígito sí está en la lista pero en una posición diferente
+        //console.log("Obtiene pica");
+        _picas++;
+      }
+    }
+
+    if (_fijas == 4) {
+      console.log("adivino");
+      this.adivinoUsuario = true;
+      this.mostrarResultado(" ¡¡ Adivinó !! ");
+    } else {
+      this.mostrarResultado("Obtuvo " + _picas + " picas y " + _fijas+ " fijas");
+    }
+    this.actualizarDatosUsuario(_fijas, _picas);
+  }
+
+  adivinar() {
     let _sUsuario: string = this.modoDosFormGroup.get("numeroAdivinarUsuario").value.toString(); // obtengo los 4 dígitos del usuario como string
     this.listaUsuario = ListaSimple.crearListaConNumero(_sUsuario);
     if(!this.listaUsuario){
       this.mostrarResultado("El número es inválido, por favor revise");
     }
     else {
-      let _picas: number = 0;
-      let _fijas: number = 0;
-      for (let index = 0; index < this.cantidadDigitos; index++) { // itero sobre cada uno de esos 4 dígitos
-        let _nUsuario = this.listaUsuario?.retornaNumeroEnPosicion(index); //tomo el dígito en esa posición y lo vuelvo un número
-        let posicionEncontrado = this.numeroMaquina?.buscarPosicionNumero(_nUsuario); // busco si en la lista está ese # y retorno la posición
-        if (posicionEncontrado == -1) { // Esto es si el dígito no está en ninguna posición de la lista
-          //console.log("No hay ni pica ni fija");
-        }
-        else if (index == posicionEncontrado) { // Esto es si el dígito está en la lista y en la misma posición
-          //console.log("Obtiene fija");
-          _fijas++;
-        } else { // Esto es si el dígito sí está en la lista pero en una posición diferente
-          //console.log("Obtiene pica");
-          _picas++;
-        }
-      }
-      this.mostrarResultado("Obtuvo " + _picas + " picas y " + _fijas + " fijas");
-      this.datosUsuario.push({
-        numeroUsuario: Number.parseInt(_sUsuario),
-        picas: _picas,
-        fijas: _fijas
-      });
-
-      if (_fijas == 4) {
-        console.log("adivino");
-      //this.cuentaTurnos = this.turnos;
-        //this.termino = true;
-        this.adivinoUsuario = true;
-        this.mostrarResultado(" ¡¡ Adivinó !! ");
-      } else {
-        //this.cuentaTurnos++;
-      }
-      this.actualizarDatosUsuario();
+      this.comparar(this.listaUsuario);
     }
-    this.adivinarMaquina();
   }
 
   adivinarMaquina(){
     this.listaMaquina = ListaSimple.crearListaConAleatorios();
   }
 
-  actualizarDatosUsuario(){
+  actualizarDatosUsuario(f: number, p: number){
+    this.datosUsuario.push({
+      numeroUsuario: this.modoDosFormGroup.get("numeroAdivinarUsuario").value,
+      picas: p,
+      fijas: f
+    });
     this.datosTablaUsuario= [];
     this.datosUsuario.forEach(dato => {
       this.datosTablaUsuario.push(dato);
